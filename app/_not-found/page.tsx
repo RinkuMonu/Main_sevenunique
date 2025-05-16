6cdfcf4bd12d338de6d256029b58a297d16b84b8
Ranjay Kumar <ranjaykumar73.rk@gmailo.com>
2025-05-16 17:35:07 +0530

    chnages in api

---
+++ app/_not-found/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      
+     <iframe
+     style={{ 
+        width: "900px",
+        height: "900px",
+      }}
+     src="https://lottie.host/embed/ca5b9605-1a03-4e36-8240-0c23d900b09c/jZDlDW4Pjy.lottie"></iframe>
+    </div>
+  );
+}
